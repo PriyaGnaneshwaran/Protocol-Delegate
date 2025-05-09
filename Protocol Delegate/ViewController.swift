0cbcd520217f8f8cebd426cc4014c737cb9ccced
PriyaGnaneshwaran <priyayogi084@gmail.com>
2025-05-09 20:37:33 +0530

    Initial Commit

---
+++ Protocol Delegate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Protocol Delegate
+//
+//  Created by Priya Gnaneshwaran on 09/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
